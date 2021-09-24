ca209cb0311474c9be3967597614ea54fcbee425
gtasa3box <gtasa3box@gmail.com>
2021-09-24 18:45:30 +0300

    g

--- src/js/global.js
+++ src/js/global.js
@@ -8,3 +8,34 @@ $('.nav__elem-dropbox').on('click', function(){
     $(this).toggleClass('active')
     $($(this).data('target')).stop(true, false).slideToggle() 
 });
+
+//inst-slider 
+$('.slider-inst').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
+});
