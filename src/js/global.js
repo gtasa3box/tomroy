d8a024c6e9f1fb90720f9abcd30da68137866fcc
gtasa3box <gtasa3box@gmail.com>
2021-09-24 16:59:06 +0300

    g

--- src/js/global.js
+++ src/js/global.js
@@ -3,3 +3,8 @@ $('#burger').on('click', function(){
     $('#burgerlink').toggleClass('active')
     $('.nav').toggleClass('active')
 });
+//dropdown
+$('.nav__elem-dropbox').on('click', function(){
+    $(this).toggleClass('active')
+    $($(this).data('target')).stop(true, false).slideToggle() 
+});
